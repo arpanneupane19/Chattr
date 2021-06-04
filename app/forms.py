460f44a8c4999fb452fe84305fae6ae76639eec8
Arpan Neupane <arpanneupane19@gmail.com>
2021-06-04 02:50:22 -0500

    password reset forms added

--- app/forms.py
+++ app/forms.py
@@ -7,9 +7,9 @@ from .main import *
 
 
 class RegisterForm(FlaskForm):
-    email = StringField("Email", validators=[InputRequired(), Email(
+    email = StringField(validators=[InputRequired(), Email(
         message="Invalid Email"), Length(max=50)], render_kw={"placeholder": "Email Address"})
-    username = StringField("Username", validators=[InputRequired(), Length(
+    username = StringField(validators=[InputRequired(), Length(
         min=4, max=15)], render_kw={"placeholder": "Username"})
     password = PasswordField(validators=[
         InputRequired(), Length(min=4)], render_kw={"placeholder": "Password (4 minimum)"})
@@ -17,8 +17,22 @@ class RegisterForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    username = StringField("Username", validators=[InputRequired(), Length(
+    username = StringField(validators=[InputRequired(), Length(
         min=4, max=15)], render_kw={"placeholder": "Username"})
     password = PasswordField(validators=[
         InputRequired(), Length(min=4)], render_kw={"placeholder": "Password (4 minimum)"})
     submit = SubmitField("Login")
+
+
+class PasswordResetRequestForm(FlaskForm):
+    email = StringField(validators=[InputRequired(), Email(
+        message="Invalid Email"), Length(max=50)], render_kw={"placeholder": "Email Address"})
+    submit = SubmitField("Request Password Reset")
+
+
+class ResetPasswordForm(FlaskForm):
+    email = StringField(validators=[InputRequired(), Email(
+        message="Invalid Email"), Length(max=50)], render_kw={"placeholder": "Email Address"})
+    password = PasswordField(validators=[
+        InputRequired(), Length(min=4)], render_kw={"placeholder": "Password (4 minimum)"})
+    submit = SubmitField("Reset Password")
