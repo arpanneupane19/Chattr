ca52698a3fabd051606a45d97832e3e768f085d7
Arpan Neupane <arpanneupane19@gmail.com>
2021-06-15 03:24:49 -0500

    fixed packages

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/arpan/Documents/ComputerProgramming/Python/Web/clone/venv"
+set -gx VIRTUAL_ENV "/Users/arpan/Documents/ComputerProgramming/Python/Web/Chattr/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
