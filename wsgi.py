2bf37153adb2151dac519a77296a6669ee36ecf2
Arpan Neupane <arpanneupane19@gmail.com>
2021-08-02 21:06:24 -0500

    wsgi file to run app

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+from app.main import socketio, app
+
+mode = 'DEV'
+
+if __name__ == '__main__' and mode == 'PRODUCTION':
+    print("Running production server.")
+    app.run()
+
+elif __name__ == "__main__" and mode == 'DEV':
+    print("Running development server.")
+    socketio.run(app, debug=True)
